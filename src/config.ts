444afc903d6ee71a52da2bb7fc9d28d61d14753e
Valodes <darkraken@live.fr>
2024-03-26 13:55:23 +0100

    🎉 New genshin fast api

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020 genshin.dev
+ * Licensed under the Open Software License version 3.0
+ */
+import path from 'path';
+
+export const dataDirectory = (type: string) =>
+  path.join(__dirname, '../public/data', type);
+export const imagesDirectory = (type: string) =>
+  path.join(__dirname, '../public/images', type);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-import { Elysia } from "elysia";
+import { Elysia, NotFoundError, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { genshin } from "./services/genshin";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .onError(({ code, error, set }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+
+      return "Not Found :(";
+    }
+  })
+  .use(genshin)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+function async(arg0: { params: { type: any } }): any {
+  throw new Error("Function not implemented.");
+}
